f80c401136e5d4fb516ab69345daf37f2b4cc77e
Brianmax <george.maxi@ucsp.edu.pe>
2024-06-25 14:56:14 -0500

    feat(querys): methods for recover data

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,50 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import { getMediaResults } from './firebase';
+import { 
+	getMediaResults,
+	getTopicWordsBetweenDates,
+	getDatesForWord
+} from './firebase';
+import { set } from 'firebase/database';
 
 function App() {
-	const [results, setResults] = useState([]);
+	const [topicWordsData, setTopicWordsData] = useState([]);
+	const [datesForWord, setDatesForWord] = useState([]);
 
+    useEffect(() => {
+        const startDate = new Date('2023-01-01T00:00:00Z');
+        const endDate = new Date('2024-06-30T23:59:59Z');
+
+        const fetchTopicWords = async () => {
+            try {
+                const data = await getTopicWordsBetweenDates(startDate, endDate);
+                setTopicWordsData(data);
+            } catch (error) {
+                console.error('Error fetching documents: ', error);
+            }
+        };
+
+        fetchTopicWords();
+    }, []);
 	useEffect(() => {
-		getMediaResults().then(setResults);
-	}, []);
-
-	return (
-		<>
-			<input type="text" />
-			<input type="date" name="" id="" />
-			<button>Buscar</button>
-			<ul>
-				{results ? (
-					results.map((result) => <li key={result.id}>{result.id}</li>)
-				) : (
-					<p>No results</p>
-				)}
-			</ul>
-		</>
-	);
+		const word = 'castillo';
+		const fetchDates = async () => {
+			try {
+				const data = await getDatesForWord(word);
+				setDatesForWord(data);
+			}
+			catch (error) {
+				console.error('Error fetching documents: ', error);
+			}
+		}
+		fetchDates();
+	}
+	, []);
+    return (
+        <div>
+            <h2>Topic Words Between Dates</h2>
+           
+        </div>
+    );
 }
-
 export default App;
--- src/firebase/getMediaResults.js
+++ src/firebase/getMediaResults.js
@@ -3,18 +3,122 @@ import {
 	getDocs,
 	limit,
 	query,
+	where
 } from 'firebase/firestore';
 import { db } from './config';
 
 const mediaResultsRef = collection(db, 'media_results');
 
-export const getMediaResults = async () => {
+const getMediaResults = async () => {
 	const q = query(mediaResultsRef, limit(3))
 	const querySnapshot = await getDocs(q);
 	const mediaResults = querySnapshot.docs.map(doc => ({
 		id: doc.id,
 		...doc.data(),
 	}));
-	console.log(mediaResults)
 	return mediaResults;
+};
+
+
+const getTopicWordsBetweenDates = async (startDate, endDate) => {
+	const q = query(mediaResultsRef, where('creationTime', '>=', '2023-01-01T00:00:00Z'), where('creationTime', '<=', '2024-06-30T23:59:59Z'));
+  try {
+    const querySnapshot = await getDocs(q);
+    const camposSeleccionados = ['creationTime', 'topicWords', 'topicViews', 'topicNums'];
+    const documents = [];
+
+    querySnapshot.forEach((doc) => {
+      const document = {};
+      camposSeleccionados.forEach((campo) => {
+        document[campo] = doc.data()[campo];
+      });
+      documents.push({
+        id: doc.id,
+        ...document
+      });
+    });
+    return documents;
+  } catch (error) {
+    console.error('Error al obtener documentos:', error);
+    return [];
+  }
+}
+
+
+const getDatesForWord = async (word) => {
+
+	const q = query(mediaResultsRef, where('topicWords.0', 'array-contains', word));
+
+	try {
+
+		const querySnapshot = await getDocs(q);
+		const camposSeleccionados = ['creationTime'];
+
+		const documents = [];
+		console.log(querySnapshot)
+		querySnapshot.forEach((doc) => {
+
+			const document = {};
+			camposSeleccionados.forEach((campo) => {
+				document[campo] = doc.data()[campo];
+			});
+			documents.push({
+				id: doc.id,
+				...document
+			});
+		}
+		);
+		return documents;
+
+	} catch (error) {
+
+		console.error('Error al obtener documentos:', error);
+		return [];
+	}
+}
+
+const getMostCommonWords = async () => {
+
+	const q = query(mediaResultsRef);
+
+	try {
+
+		const querySnapshot = await getDocs(q);
+		const camposSeleccionados = ['topicWords.0'];
+
+		const documents = [];
+
+		querySnapshot.forEach((doc) => {
+			
+			const document = {};
+			camposSeleccionados.forEach((campo) => {
+				document[campo] = doc.data()[campo];
+			});
+			documents.push({
+				id: doc.id,
+				...document
+			});
+		}
+		);
+
+		const words = documents.map(doc => doc.topicWords).flat();
+		const wordCounts = words.reduce((acc, word) => {
+			acc[word] = (acc[word] || 0) + 1;
+			return acc;
+		}, {});
+
+		const sortedWords = Object.entries(wordCounts).sort((a, b) => b[1] - a[1]);
+		return sortedWords;
+
+	} catch (error) {
+		
+		console.error('Error al obtener documentos:', error);
+		return [];
+	}	
+}
+export {
+    getMediaResults,
+	getTopicWordsBetweenDates,
+	getDatesForWord,
+	getMostCommonWords
 };
